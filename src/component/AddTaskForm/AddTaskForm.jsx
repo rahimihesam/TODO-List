5210f55ce01d826129bc01636ecd2a3224926745
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-23 14:06:04 +0330

    Add AddTaskForm component.

---
+++ src/component/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const AddTaskForm = () => (
+    <div className="AddTaskForm">
+        <form action="">
+            <input type="text" placeholder="Write"/>
+            <button type="submit">Add</button>
+        </form>
+    </div>
+)
+
+export default AddTaskForm
