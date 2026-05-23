47230d6a50367d21743511f0762237a542e2cb33
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-23 14:05:46 +0330

    Add TaskList component.

---
+++ src/component/TaskList/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const TaskList = () => (
+    <div className="TaskList">
+        <ul>
+            <li>
+                <input type="checkbox"/>
+                <h2>Build</h2>
+                <button>Delete</button>
+            </li>
+        </ul>
+    </div>
+)
+export default TaskList
