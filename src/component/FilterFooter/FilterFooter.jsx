baaf8b99b982f679317640270782ba3d9ad7a8d4
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-23 14:06:14 +0330

    Add FilterFooter component.

---
+++ src/component/FilterFooter/FilterFooter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FilterFooter = () => (
+    <div className="FilterFooter">
+        <div>
+            2 items
+        </div>
+        <ul>
+            <li>
+                <button>All</button>
+            </li>
+            <li>
+                <button>Active</button>
+            </li>
+            <li>
+                <button>Completed</button>
+            </li>
+        </ul>
+    </div>
+)
+export default FilterFooter
