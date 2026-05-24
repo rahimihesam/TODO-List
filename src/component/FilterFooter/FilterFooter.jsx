2954854e414b322eb511e4d23fb324e234747212
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-24 10:38:36 +0330

    Completed Style part.

--- src/component/FilterFooter/FilterFooter.jsx
+++ src/component/FilterFooter/FilterFooter.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 
 const FilterFooter = () => (
-    <div className="FilterFooter">
-        <div>
+    <div className="w-full flex bg-green-200 p-3">
+        <div className="w-full flex items-center">
             2 items
         </div>
-        <ul>
-            <li>
-                <button>All</button>
-            </li>
-            <li>
-                <button>Active</button>
-            </li>
-            <li>
-                <button>Completed</button>
-            </li>
-        </ul>
+        <div className="w-full">
+            <ul className="w-full flex items-center justify-end flex-row h-9">
+                <li>
+                    <button className="p-2 hover:bg-white transition cursor-pointer active:bg-blue-500">All</button>
+                </li>
+                <li>
+                    <button className="p-2 hover:bg-white transition cursor-pointer active:bg-blue-500">Active</button>
+                </li>
+                <li>
+                    <button className="p-2 hover:bg-white transition cursor-pointer active:bg-blue-500">Completed</button>
+                </li>
+            </ul>
+        </div>
     </div>
 )
 export default FilterFooter
