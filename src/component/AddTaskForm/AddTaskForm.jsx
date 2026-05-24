773b26ca7527e6f180f6defe589c5e2afd3618a5
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-24 09:24:20 +0330

    Changed styles with Tailwind.

--- src/component/AddTaskForm/AddTaskForm.jsx
+++ src/component/AddTaskForm/AddTaskForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './AddTaskForm.css'
+import '../../styles/AddTaskForm.css'
 const AddTaskForm = () => (
-    <div className="AddTaskForm">
-        <form action="">
-            <input type="text" placeholder="Write"/>
-            <button type="submit">Add</button>
+    <div className="w-full mb-8">
+        <form action="" className="flex">
+            <input type="text" className="w-full h-12 text-2xl border-green-200 border p-1 focus:border-green-500 outline-none" placeholder="Write"/>
+            <button type="submit" className="h-12 text-2xl border border-green-50 bg-green-200 hover:border-green-500 hover:bg-white transition">Add</button>
         </form>
     </div>
 )
