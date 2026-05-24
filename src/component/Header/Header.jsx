e77b99ab11f61e9ecd3b0eec623209bc249f6c37
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-24 09:24:15 +0330

    Changed styles with Tailwind.

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Header.css'
+import '../../styles/Header.css'
 const Header = () => (
-    <header className="Header">
+    <header className="text-center text-2xl min-h-28 m-auto flex items-center justify-center">
         <h2>My ToDos</h2>
     </header>
 )
